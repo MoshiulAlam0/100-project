9bb4667846108275e977429156149e479f54bcfa
MoshiulAlam0 <allenshvro@gmail.com>
2024-05-02 11:30:16 +0600

    modifty

--- frist-big-num-form the arry/finalCodeBigNum.js
+++ frist-big-num-form the arry/finalCodeBigNum.js
@@ -4,16 +4,16 @@ function findBigNum (still_muchNumber){
         All_big_nums : [], 
         your_big_nums : []
     }
-    for (let index = 0; index < array.length; index++) {
-        const maxNum = Math.max(...array);
-        result.All_big_nums.push(maxNum);
+    for (let index = 0; index < array.length; index++) { // stap1: loop has been thrown still array length
+        const maxNum = Math.max(...array); //stap2: find big number
+        result.All_big_nums.push(maxNum); // step3 : set big number insede obj property
 
-        let maxNumberIndex = array.indexOf(maxNum);
+        let maxNumberIndex = array.indexOf(maxNum); // step4: get max num index for set 0 insted of max num
         array[maxNumberIndex] = 0;
     }
-    if(still_muchNumber <= array.length){
+    if(still_muchNumber <= array.length){ // step5: The still_muchNumber should not exceed the length of the array
         let slice = result.All_big_nums.slice(0, still_muchNumber);
-        result.your_big_nums = [...slice];
+        result.your_big_nums = [...slice]; // step6: set slice array inside the object property 
     }
     return result;
 }
